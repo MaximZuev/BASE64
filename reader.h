9ed0046c84408159e7ef4d6c31234ae17293eb91
MaximZuev <43428953+MaximZuev@users.noreply.github.com>
2019-05-06 18:27:49 +0700

    Create reader.h

---
+++ reader.h
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#ifndef B64_READER_H
+#define B64_READER_H
+int readinput(int argc, char **argv, char **in, char **out, int **options, FILE **outfile, FILE **infile );
+#endif //B64_READER_H
